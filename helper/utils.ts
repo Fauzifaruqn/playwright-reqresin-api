d5a3eed1245f6942684786a6bffdd70e2970de8a
Fauzifaruqn <Fauzifaruqn@users.noreply.github.com>
2024-10-27 02:57:11 +0000

    deploy: e9f15a4de833fd01b8b5a1b2afaf50a187a68ab5

--- helper/utils.ts
+++
@@ -1,6 +0,0 @@
-export const logResponse = async (response: any) => {
-  console.log(`Status: ${response.status()}`);
-  
-  const responseBody = await response.json(); // Parse the response body as JSON
-  console.log(`Response Body: ${JSON.stringify(responseBody, null, 2)}`); // Convert object to JSON string with pretty printing
-};
